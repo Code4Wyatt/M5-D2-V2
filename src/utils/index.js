d181dbbba69da308d9a9c5d8b510dd74dc720b12
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-02 15:19:27 +0000

    Updated

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,3 +12,15 @@ export const authorsFilePath = join(dirname(fileURLToPath(import.meta.url)), "..
 
 export const blogsFilePath = join(dirname(fileURLToPath(import.meta.url)), "../../services/blogs/blogs.json")
 
+const publicDirectory = path.join(__dirname, "../../../public")
+
+export const parseFile = multer()
+
+export const uploadFile = (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
+}
+
