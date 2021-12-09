a5c82c075de852fb71d914befd5846deff39f051
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-09 16:44:40 +0000

    Update

--- src/utils/email/email-tools.js
+++ src/utils/email/email-tools.js
@@ -11,5 +11,9 @@ export const sendPostEmail = async recipientAddress => {
         html: "Welcome to our blog site, thank you for making your first post",
     }
 
-    await sgMail.send(email)
+    await sgMail.send(email).then(() => {
+        console.log(`Post email sent!`)
+    }).catch((error) => {
+        console.log(error)
+    })
 }
